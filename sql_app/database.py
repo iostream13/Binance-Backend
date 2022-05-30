746c3961bcbf7a8793554c936abb6aaa05455d09
iostream13 <20020013@vnu.edu.vn>
2022-05-30 08:53:38 +0700

    add pool size and max overflow in engine

--- sql_app/database.py
+++ sql_app/database.py
@@ -6,6 +6,8 @@ sqlalchemy_database_url = "mysql+mysqlconnector://root:hien1308@localhost:3306/b
 
 engine = create_engine(
     sqlalchemy_database_url,
+    pool_size=15, 
+    max_overflow=20
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
